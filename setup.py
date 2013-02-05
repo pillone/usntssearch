2f5b0878fdfcc2751463b08602b0639893e3bebb
pillone <pillone@inbox.com>
2013-02-05 22:04:00 +0100

    pok

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NZBmegasearch',
+    version='0.1',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask', 'requests']
+)
