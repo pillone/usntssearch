a30bfb5a118bbb159bd56b8902adda7989791886
pillone <pillone@inbox.com>
2013-05-19 22:08:59 +0200

    mx

--- NZBmegasearch/install_openshift.sh
+++ NZBmegasearch/install_openshift.sh
@@ -3,8 +3,6 @@ echo '*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*'
 echo '               NZBMEGASEARCH install for Openshift'
 echo '*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*' 
 
-rm OPENSHIFT_REPO_DIR/wsgi -rf
-
 echo 'Running update -- 01/02'
 git fetch
 
@@ -12,11 +10,11 @@ echo 'Running update -- 02/02'
 git reset --hard origin/master
 
 echo 'Preparing...'
-cp builtin_params.ini OPENSHIFT_DATA_DIR
+cp builtin_params.ini $OPENSHIFT_DATA_DIR
+cp openshift/application .
+cp openshift/app.py $OPENSHIFT_REPO_DIR/
 
-echo 'Copying directory to wsgi...'
-cp builtin_params.ini OPENSHIFT_DATA_DIR
 
-cp -r $PWD OPENSHIFT_REPO_DIR/wsgi
+cp -r $PWD $OPENSHIFT_REPO_DIR/wsgi
 
 ctl_app restart
